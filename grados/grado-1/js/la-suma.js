14faee9fa862b556da38cc488a174467a9c7dfa2
Raul CF <elmonoska@outlook.com>
2023-02-03 12:59:03 -0600

    Agregando contenido y  funcionalidades a pagina la-suma primer grado

--- grados/grado-1/js/la-suma.js
+++ grados/grado-1/js/la-suma.js
@@ -9,5 +9,38 @@ function crea5Ejercicios1a20 () {
     }
 }
 
+const listaSuma10Ejercicios = document.querySelector('#suma10Ejercicios');
+for(let i = 1; i <= 5; i++){
+    let numero1 = numeroAleatorio(1,99);
+    let numero2 = numeroAleatorio(1,99);
+    while(numero1 + numero2 >= 100){
+        numero1 = numeroAleatorio(1,99);
+        numero2 = numeroAleatorio(1,99);
+    }
+    let suma = numero1 + numero2;
+    const li = document.createElement('li');
+    li.classList = 'list-none p-3';
+    
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.classList = 'input-group-text p-3 shadow w-100 mx-auto';
+    input.placeholder = 'Escribe tu respuesta aqui';
+    listaSuma10Ejercicios.appendChild(li).innerHTML = `${numero1} <span class="align-middle text-math-danger fs-2">+</span> ${numero2}`;
+
+    listaSuma10Ejercicios.appendChild(input);
+    //Reconoce y evalua las respuesta ingresadas por el usuario
+    input.addEventListener('blur', () => {
+        if(parseInt(input.value) !== suma){
+            input.classList.add('border', 'border-danger', 'bg-danger', 'bg-opacity-25')
+            input.placeholder = 'Respuesta incorrecta, intentalo nuevamente'
+            console.log('mal')
+        }else{
+            input.classList.replace('border-danger', 'border-success');
+            input.classList.replace('bg-danger', 'bg-success');
+        }
+    })
+
+}
+
 //Ejecucion de funciones en DOM
 crea5Ejercicios1a20();
