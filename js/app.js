c80e2d2798c31d56628c41135c808d8f367bf010
Raul CF <elmonoska@outlook.com>
2023-03-03 11:07:19 -0600

    Validando errores de service cambiando ruta

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 //Valida si el navegador puede trabajar con service worker
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../serviceWorker.js')
+    const pathServiceWorker = './serviceWorker.js'
+    navigator.serviceWorker.register(pathServiceWorker)
     .then(registro => console.log('Registro de SW correcto', registro))
-    .catch(error => alert('Error al tratar de registrar el SW', error))
+    .catch(error => {
+        alert('Error al tratar de registrar el SW', error);
+        console.error('Error al tratar de registrar el SW', error);
+    })
 }
