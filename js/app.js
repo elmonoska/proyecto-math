67d484961d66bd6a79b243c7ce25275925d27e24
Raul CF <elmonoska@outlook.com>
2023-03-03 10:49:41 -0600

    Corrigiendo anymaskable y soporte para ios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+//Valida si el navegador puede trabajar con service worker
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('../serviceWorker.js')
     .then(registro => console.log('Registro de SW correcto', registro))
-    .catch(error => console.error('Error al tratar de registrar el SW', error))
+    .catch(error => alert('Error al tratar de registrar el SW', error))
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -142,4 +142,3 @@ self.addEventListener('fetch', e => {
         })
     )
 })
-
