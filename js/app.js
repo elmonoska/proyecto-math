ce1c65613237d13dce914c31e5f0f6e0c7bd5090
Raul CF <elmonoska@outlook.com>
2023-03-03 11:21:55 -0600

    Cambio service

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //Valida si el navegador puede trabajar con service worker
 if('serviceWorker' in navigator){
     const pathServiceWorker = './serviceWorker.js'
+    app.get(pathServiceWorker, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./", "serviceWorker.js"));
+      });
     navigator.serviceWorker.register(pathServiceWorker)
     .then(registro => console.log('Registro de SW correcto', registro))
     .catch(error => {
